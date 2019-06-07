aa72a2f39b096018571233a8dd31d92a06e99c49
web3giay <48266603+web3giay@users.noreply.github.com>
2019-06-08 01:04:23 +0700

    Update JScript.js

--- Content/JScript/JScript.js
+++ Content/JScript/JScript.js
@@ -403,9 +403,9 @@ $(document).ready(function() {
 
     $(window).scroll(function () {
         //Fix detail
-        var vscrollC = 800;
+        var vscrollC = 1220;
       
-        var vscrollL = $('.footer').height();
+        var vscrollL = 1484;
         //alert($(this).scrollTop() + "-" + vscrollC); //1220-1484, 1220-1484
         if ($(this).scrollTop() > vscrollL) {
             if ($(this).scrollTop() < vscrollC) {
